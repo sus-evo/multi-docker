16bf6718d53ad9a06a80318de3fcf91f3a22a632
test <test@localhost.localdomain>
2022-05-24 23:55:13 +0900

    change 20220524_06

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,6 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-header">ようこそ！React 2022年5月24日 01</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -20,10 +19,11 @@ function App() {
           >
             Fib Calculator
           </a>
-          <Link to="/">Home</Link>
+          <Link to="/">Ho111111me</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
+          <h1 className="App-header">ようこそ！React 2022年5月24日 01</h1>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
         </div>
